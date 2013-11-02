b0e5a512f3119038d51384760a12c715b3327979
SY Quek <quekshuy@gmail.com>
2013-11-03 03:23:28 +0800

    With JSONP.

--- captcha_api/views.py
+++ captcha_api/views.py
@@ -2,8 +2,25 @@ from recaptcha.client import captcha
 
 from django.views.generic.base import View
 from django.conf import settings
+from django.utils import simplejson as json
 
-class CaptchaAPIView(object):
+
+class CaptchaAPIView(View):
+
+    def get_recaptcha_response(
+                            self, 
+                            challenge_field, 
+                            response_field, 
+                            remote_addr, 
+                            private_key):
+
+        return captcha.submit(
+                challenge_field,
+                response_field,
+                private_key,
+                remote_addr
+                )
+
 
     def post(self, request):
 
@@ -17,12 +34,11 @@ class CaptchaAPIView(object):
         if None in (challenge_field, response_field):
             return HttpResponse(status=400)
 
-        response = captcha.submit(
-                challenge_field,
-                response_field,
-                private_key,
-                remote_addr
+        response = self.get_recaptcha_response(
+                challenge_field, response_field, remote_addr,
+                private_key
                 )
+
         if response.is_valid:
             return HttpResponse(status=200)
 
@@ -31,4 +47,38 @@ class CaptchaAPIView(object):
                 content='Invalid CAPTCHA'
                 )
 
+
+class CaptchaAPIJSONPView(CaptchaAPIView):
+
+    def get(self, request):
+
+        challenge_field, response_field = \
+                request.GET.get('recaptcha_challenge_field'),\
+                request.GET.get('recaptcha_response_field')
+
+        remote_addr = request.META['REMOTE_ADDR']
+        private_key = settings.RECAPTCHA_PRIVATE_KEY
+
+        callback = request.GET.get('callback')
+
+        if None in (challenge_field, response_field, callback):
+            return HttpResponse(status=400)
+
+        response = self.get_recaptcha_response(
+                challenge_field, response_field, remote_addr,
+                private_key
+                )
+
+        if response.is_valid:
+            return HttpResponse(
+                    status=200,
+                    content=''.join([callback, '(', \
+                            json.dumps({
+                                    'result': 'success'
+                                }), ')'])
+                            )
+        return HttpResponse(
+                status=403, 
+                content='Invalid CAPTCHA'
+                )
 
--- easy_captcha/urls.py
+++ easy_captcha/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import patterns, include, url
 
-from captcha_api.views import CaptchaAPIView
+from captcha_api.views import CaptchaAPIView, CaptchaAPIJSONPView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+
     url(r'^captcha_api/verify$', CaptchaAPIView.as_view()),
+
+    url(r'^captcha_api/jsonp_verify$', CaptchaAPIJSONPView.as_view()),
+
 )
